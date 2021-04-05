217f2b0e22f79deb2d8dd721ae56365a302739ac
osnir costa <osnirmaster@hotmail.com>
2021-04-05 14:40:36 -0300

    frontend

---
+++ angular-login/src/app/app.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { AccountService } from './_services';
+import { User } from './_models';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+    user: User;
+
+    constructor( private router:Router , private accountService: AccountService) {
+        this.accountService.user.subscribe(x => this.user = x);
+        this.router.initialNavigation();
+    }
+
+    logout() {
+        this.accountService.logout();
+    }
+}
---
+++ angular-login/src/app/app.module.ts
@@ -0,0 +1,52 @@
+﻿import { Injector, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
+import { HomeComponent } from './home';
+
+import { createCustomElement } from '@angular/elements';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    entryComponents: [
+        AppComponent
+      ]
+})
+export class AppModule { 
+
+    constructor(private intector:Injector){
+
+    }
+
+    ngDoBootstrap(){
+
+        const elem = createCustomElement(AppComponent, {injector: this.intector});
+        customElements.define('micro-fe-login', elem);
+    };
+};
