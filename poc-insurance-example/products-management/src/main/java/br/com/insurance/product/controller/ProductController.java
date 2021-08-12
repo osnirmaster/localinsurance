101239446e09fb48012df162291d3adf21e63cdd
Osnir Teixeira Costa <osnirteixeiracosta@MacBook-Air-de-Osnir.local>
2021-08-12 01:14:53 -0300

    add clean arch layers up

--- poc-insurance-example/products-management/src/main/java/br/com/insurance/product/controller/ProductController.java
+++ poc-insurance-example/products-management/src/main/java/br/com/insurance/product/controller/ProductController.java
@@ -1,6 +1,19 @@
 package br.com.insurance.product.controller;
 
+import br.com.insurance.product.domain.usecase.GetBusinessProduct;
+import br.com.insurance.product.domain.usecase.RegisterBusinessProduct;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+    private final RegisterBusinessProduct registerBusinessProduct;
+    private final GetBusinessProduct getBusinessProduct;
+
+
+    public ProductController(RegisterBusinessProduct registerBusinessProduct, GetBusinessProduct getBusinessProduct) {
+        this.registerBusinessProduct = registerBusinessProduct;
+        this.getBusinessProduct = getBusinessProduct;
+    }
 
 
 }
