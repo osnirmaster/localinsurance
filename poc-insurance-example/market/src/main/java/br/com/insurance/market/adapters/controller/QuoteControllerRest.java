123c91af0d0d16593fafb33f1faed0a080e2e45f
Osnir Teixeira Costa <osnirteixeiracosta@MacBook-Air-de-Osnir.local>
2021-11-24 00:41:22 -0300

    poc cotacao async sync

--- poc-insurance-example/market/src/main/java/br/com/insurance/market/adapters/controller/QuoteControllerRest.java
+++ poc-insurance-example/market/src/main/java/br/com/insurance/market/adapters/controller/QuoteControllerRest.java
@@ -3,6 +3,7 @@ package br.com.insurance.market.adapters.controller;
 import br.com.insurance.market.adapters.dto.RequestQuote;
 import br.com.insurance.market.adapters.dto.ResponseQuote;
 import br.com.insurance.market.domain.Quote;
+import br.com.insurance.market.usecase.GetQuoteCalculation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/insurance/quote")
 public class QuoteControllerRest {
 
+    private final GetQuoteCalculation getQuoteCalculation;
+
+    public QuoteControllerRest(GetQuoteCalculation getQuoteCalculation) {
+        this.getQuoteCalculation = getQuoteCalculation;
+    }
+
     @PostMapping
     public ResponseEntity<ResponseQuote> price(@RequestBody RequestQuote request, UriComponentsBuilder uriBuilder){
-        Quote quote =
+        Quote quote = getQuoteCalculation.insuranceQuote()
 
         URI uri = uriBuilder.path("/quote/{id}").buildAndExpand(productSaved.getProductId()).toUri();
         return ResponseEntity.created(uri).body(new ProductDto(productSaved))
--- poc-insurance-example/market/src/main/java/br/com/insurance/market/adapters/dto/RequestQuote.java
+++ poc-insurance-example/market/src/main/java/br/com/insurance/market/adapters/dto/RequestQuote.java
@@ -1,6 +1,7 @@
 package br.com.insurance.market.adapters.dto;
 
 import br.com.insurance.market.domain.CreditContract;
+import br.com.insurance.market.domain.Quote;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,4 +15,55 @@ public class RequestQuote {
     private String idCustomer;
     private List<CreditContract> contracts;
 
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public LocalDate getDataQuota() {
+        return dataQuota;
+    }
+
+    public void setDataQuota(LocalDate dataQuota) {
+        this.dataQuota = dataQuota;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCustomerClient() {
+        return customerClient;
+    }
+
+    public void setCustomerClient(String customerClient) {
+        this.customerClient = customerClient;
+    }
+
+    public String getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(String idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public List<CreditContract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(List<CreditContract> contracts) {
+        this.contracts = contracts;
+    }
+
+    public Quote convertTo(){
+        return new Quote();
+    }
 }
